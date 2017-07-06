27395a8674781924d5bd7f23c553f32539864b70
yoichi kobayashi <info@tplh.net>
2017-07-06 13:19:00 +0900

    [add] Hover class and set trigger of hover effects.

--- src/js/init/index.js
+++ src/js/init/index.js
@@ -1,4 +1,8 @@
-// import Preloader from '../modules/preloader.js';
+import Hover from 'js-util/Hover.js';
 
 export default function() {
+  const elmHover = document.getElementsByClassName('js-hover');
+  for (var i = 0; i < elmHover.length; i++) {
+    new Hover(elmHover[i]);
+  }
 };
