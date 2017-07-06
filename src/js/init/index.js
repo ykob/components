dc0632615c38a002638e4d3e79b0232b44b9c938
yoichi kobayashi <info@tplh.net>
2017-07-06 15:25:32 +0900

    [add] hamburger component.

--- src/js/init/index.js
+++ src/js/init/index.js
@@ -1,8 +1,23 @@
+import Vue from 'vue/dist/vue.min';
 import Hover from 'js-util/Hover.js';
 
 export default function() {
+  // マウスオン制御用クラスの初期化
   const elmHover = document.getElementsByClassName('js-hover');
   for (var i = 0; i < elmHover.length; i++) {
     new Hover(elmHover[i]);
   }
+
+  // Hamburgerボタン
+  const vueHamburger = new Vue({
+    el: '#hamburger',
+    data: {
+      isOpenedNavi: false,
+    },
+    methods: {
+      toggleNavi: function() {
+        this.isOpenedNavi = !this.isOpenedNavi;
+      }
+    }
+  })
 };
