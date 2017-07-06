45a4b8ca63043467a9428d1fc345bb16cc46883b
yoichi kobayashi <info@tplh.net>
2017-07-06 13:18:01 +0900

    [add] initialize scroll manager.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,17 @@
 import initIndex from './init/index.js'
 
+import ScrollManager from './modules/ScrollManager.js';
+
 const pageId = document.getElementsByClassName('l-page')[0].getAttribute('data-page-id');
 
 const init = () => {
+  const scrollManager = new ScrollManager();
   switch (pageId) {
     case 'index':
       initIndex();
       break;
     default:
   }
+  scrollManager.scroll();
 }
 init();
